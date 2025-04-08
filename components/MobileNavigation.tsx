289909ca69b19fe7b39906a8a58cf3d1d3701835
Rudy Nurafif <rudynurafif@gmail.com>
2025-04-09 05:54:05 +0700

    Sign In and Sign Out

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -16,6 +16,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import FileUploader from "./FileUploader";
 import { Button } from "./ui/button";
+import { signOutUser } from "@/lib/actions/users.action";
 
 interface Props {
   ownerId: string;
@@ -109,7 +110,7 @@ const MobileNavigation = ({
             <Button
               type="submit"
               className="mobile-sign-out-button"
-              onClick={() => {}}
+              onClick={async () => await signOutUser()}
             >
               <Image
                 src="/assets/icons/logout.svg"
